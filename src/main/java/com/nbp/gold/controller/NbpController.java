8089f9314cd1bdd337d4a69f35d1a4a2866ab3d1
Joanna Walkiewicz <asia-walkiewicz@wp.pl>
2021-07-04 18:30:38 +0200

    adding swagger

--- src/main/java/com/nbp/gold/controller/NbpController.java
+++ src/main/java/com/nbp/gold/controller/NbpController.java
@@ -1,13 +1,14 @@
 package com.nbp.gold.controller;
 
 import com.nbp.gold.services.NbpService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/nbp")
@@ -19,9 +20,13 @@ public class NbpController {
         this.nbpService = nbpService;
     }
 
+    @ApiOperation(value = "Get gold rate for selected dates", response = Double.class,
+            notes = "Endpoint to get gold rate for selected dates. Response: average of gold rates in selected dates")
     @GetMapping(value="/goldRates", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Double> calculateGoldRates(
+            @ApiParam(name ="from", type = "LocalDate", required = true)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @ApiParam(name ="to", type = "LocalDate", required = true)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
     ){
         Double goldRates = nbpService.calculateGoldRates(from, to);
