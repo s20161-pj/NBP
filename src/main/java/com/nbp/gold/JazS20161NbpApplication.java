657578e201a8d0d155af9a407cd213524ad9af93
Joanna Walkiewicz <asia-walkiewicz@wp.pl>
2021-07-04 16:38:54 +0200

    spring initializr

---
+++ src/main/java/com/nbp/gold/JazS20161NbpApplication.java
@@ -0,0 +1,13 @@
+package com.nbp.gold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS20161NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS20161NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbp/gold/JazS20161NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbp.gold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS20161NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
